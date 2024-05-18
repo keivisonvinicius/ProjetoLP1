b38664c40f10618f9cc7d5fcc345db5bb9427166
ViniciusSilas01 <vinicius2012silas@gmail.com>
2024-05-18 11:30:29 -0300

    incrementando ao projeto a função adicionar horário a tarefa

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function addTask() {
     var title = document.getElementById("title").value;
     var description = document.getElementById("description").value;
+    var time = document.getElementById("time").value;
 
-    if (title === "" || description === "") {
+    if (title === "" || description === "" || time === "") {
         alert("Por favor, preencha todos os campos.");
         return;
     }
@@ -13,16 +14,20 @@ function addTask() {
     var cell1 = row.insertCell(0);
     var cell2 = row.insertCell(1);
     var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
 
     cell1.innerHTML = title;
     cell2.innerHTML = description;
-    cell3.innerHTML = '<button onclick="deleteTask(this)">Excluir</button>';
+    cell3.innerHTML = time;
+    cell4.innerHTML = '<button onclick="deleteTask(this)">Excluir</button>';
 
     document.getElementById("title").value = "";
     document.getElementById("description").value = "";
+    document.getElementById("time").value = "";
 }
 
 function deleteTask(btn) {
     var row = btn.parentNode.parentNode;
     row.parentNode.removeChild(row);
 }
+
